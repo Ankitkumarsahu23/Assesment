2e45dd0dda81e3e83b3826ed5e395f3eb18c2463
Ankitkumarsahu23 <108142791+Ankitkumarsahu23@users.noreply.github.com>
2022-08-10 19:17:37 +0530

    Add files via upload

--- PII/src/PIItest.java
+++ PII/src/PIItest.java
@@ -11,13 +11,17 @@ public class PIItest {
 		 while((str = b.readLine())!= null) {
 			 
 			 StringTokenizer stn = new StringTokenizer(str);
-			 String Roll_No = stn.nextToken();
+			 
 			 String Email = stn.nextToken();
+			 String dob = stn.nextToken();
+			 String Card_No = stn.nextToken();
 			 String Aadhar_No = stn.nextToken();
 			 String  Mobile_No = stn.nextToken();
 			 			 
-			 System.out.println("Roll_No - " + Roll_No);
+
 			 System.out.println("Email id - " + Email);
+			 System.out.println("Date of Birth - " + dob);
+			 System.out.println("Card_No - " + Card_No);
 			 System.out.println("Aadhar_No - " + Aadhar_No);
 			 System.out.println("Mobile_No - " + Mobile_No);
 			 System.out.println("***********************" );
